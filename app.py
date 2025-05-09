a63ad35f98341201ec0bad9b7f56febbadd555cb
G.Mohanraj <150698649+Mohanrajx@users.noreply.github.com>
2025-05-09 16:11:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from nrclex import NRCLex
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    emotions = {}
+    top_emotions = []
+    if request.method == 'POST':
+        text = request.form['text']
+        if text.strip():
+            emotion = NRCLex(text)
+            emotions = emotion.raw_emotion_scores
+            top_emotions = emotion.top_emotions
+    return render_template('index.html', emotions=emotions, top_emotions=top_emotions)
+
+if __name__ == '__main__':
+    app.run(debug=True)
